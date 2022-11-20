e4a3c1a5cdd462c4daaff6ba33a4e33855de5907
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2022-11-20 13:55:52 +0900

    initiate

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get("/hello")
+  hello(): string {
+    return "Hello everyone";
+  }
+}
